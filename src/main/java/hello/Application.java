1f96c3a38b8c2cc80e5f4e28c0182860bfdae7d5
acournoyer1 <cournoyer1@gmail.com>
2019-02-06 10:47:04 -0500

    Added tests and travis

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -16,43 +17,16 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
     public CommandLineRunner demo(BuddyInfoRepository repository) {
         return (args) -> {
 
-//            // save a couple of customers
-//            repository.save(new BuddyInfo("Jack", 15));
-//            repository.save(new BuddyInfo("Chloe", 16));
-//            repository.save(new BuddyInfo("Kim", 20));
-//            repository.save(new BuddyInfo("David", 30));
-//            repository.save(new BuddyInfo("Michelle", 12));
-//
-//            // fetch all customers
-//            log.info("Customers found with findAll():");
-//            log.info("-------------------------------");
-//            for (BuddyInfo customer : repository.findAll()) {
-//                log.info(customer.toString());
-//            }
-//            log.info("");
-//
-//            // fetch an individual customer by ID
-//            repository.findById(1L)
-//                    .ifPresent(customer -> {
-//                        log.info("Customer found with findById(1L):");
-//                        log.info("--------------------------------");
-//                        log.info(customer.toString());
-//                        log.info("");
-//                    });
-//
-//            // fetch customers by last name
-//            log.info("Customer found with findByLastName('Bauer'):");
-//            log.info("--------------------------------------------");
-//            repository.findByName("Bauer").forEach(bauer -> {
-//                log.info(bauer.toString());
-//            });
-//            // for (Customer bauer : repository.findByLastName("Bauer")) {
-//            // 	log.info(bauer.toString());
-//            // }
-//            log.info("");
+            // save a couple of customers
+            repository.save(new BuddyInfo("Jack", 15));
+            repository.save(new BuddyInfo("Chloe", 16));
+            repository.save(new BuddyInfo("Kim", 20));
+            repository.save(new BuddyInfo("David", 30));
+            repository.save(new BuddyInfo("Michelle", 12));
         };
     }
 
